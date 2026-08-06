fdaef93625dfc1361eb5a912679ad1827ce2af8d
Chirag Chanchal <chiragchanchal2345@gmail.com>
2026-08-06 16:08:14 +0530

    first class

---
+++ 1class/app.js
@@ -0,0 +1,17 @@
+// const fs = require('fs');
+// const data = fs.readFileSync('notes.txt', 'utf-8');
+// console.log(data);
+// fs.writeFileSync('notes.txt', 'This file is created by Node.js');
+
+
+// const path = require('path');
+// const filePath = path.join(__dirname, 'test', 'hello.txt');
+// console.log(filePath);
+
+// const os = require('os');
+// const userInfo = os.userInfo();
+// console.log(userInfo);
+
+
+const des = require('./data/des');
+console.log(des);
---
+++ 1class/data/des.js
@@ -0,0 +1,19 @@
+// const des = [
+//     "Goa",
+//     "Kerala",
+//     "Rajasthan"
+// ];
+
+// module.exports = des;
+
+const packages = [
+    {
+        name: "package1",
+        version : "1.0.0."
+    },
+    {
+        name: "package2",
+        version : "2.0.0."
+    }
+]
+module.exports = packages;
