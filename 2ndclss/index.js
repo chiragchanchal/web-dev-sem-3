a242205a58a8df96b7337f9956ed309148071ff7
Chirag Chanchal <chiragchanchal2345@gmail.com>
2026-08-18 15:54:29 +0530

    web dev 18th august updated

--- 2ndclss/index.js
+++ 2ndclss/index.js
@@ -20,7 +20,15 @@ app.get("/packages/:id", (req, res) => {
     const packageId = parseInt(req.params.id);
     const tourPackage = packages.find((pkg) => pkg.id === packageId);
     res.send(tourPackage);
+    res.status(200).send(tourPackage);
 });    
+
+app.post("/packages", (req, res) => {
+    const newPackage = req.body;
+    res.json(newPackage);
+    // packages.push(newPackage);
+    // res.status(201).send(newPackage);
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
