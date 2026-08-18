b692935be5aa54b97361e403e8b72e6bf6f5af12
Chirag Chanchal <chiragchanchal2345@gmail.com>
2026-08-18 15:36:36 +0530

    web dev 18th august

---
+++ 2ndclss/data/tour.js
@@ -0,0 +1,27 @@
+const packages = [
+    {
+        id: 1,
+        name: "Tour Package 1",
+        description: "This is the first tour package.",
+        duration: "5 days",
+        image: "tour1.jpg",
+        destination: "Goa"
+    },
+    {
+        id: 2,
+        name: "Tour Package 2",
+        description: "This is the second tour package.",
+        duration: "5 days",
+        image: "tour2.jpg",
+        destination: "Rome"
+    },
+    { 
+        id: 3,
+        name: "Tour Package 3",
+        description: "This is the third tour package.",
+        duration: "5 days",
+        image: "tour3.jpg",
+        destination: "London"
+    }
+]
+module.exports = packages;
